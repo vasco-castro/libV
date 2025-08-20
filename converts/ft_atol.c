e46630d82d4cc0a8d082392d3b766a85c976083b
Vasco Castro <vcastro@gmtel.pt>
2025-08-20 15:24:00 +0100

    Add comment documentation to convert functions, and isolated ft_issign

--- converts/ft_atol.c
+++ converts/ft_atol.c
@@ -1,32 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vsoares- <vsoares-@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/12/31 18:16:00 by vsoares-          #+#    #+#             */
-/*   Updated: 2025/02/23 16:33:56 by vsoares-         ###   ########.fr       */
+/*   Created: 2025/03/05 14:20:09 by vsoares-          #+#    #+#             */
+/*   Updated: 2025/03/05 14:20:09 by vsoares-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/libft.h"
 
 /**
- * @brief Converts the initial portion of the string pointed to by 'str' to
- * 		  an int.
+ * @brief Converts the initial portion of the string pointed to by 'str' to a
+ * 		  long int.
  *
  * This function skips leading whitespace, handles optional '+' or '-' signs,
- * and converts the subsequent numeric characters into an integer value.
+ * and converts the subsequent numeric characters into a long integer value.
  *
  * @param str The string to convert.
- * @return The integer value represented by the string.
+ * @return The long integer value represented by the string.
  */
-int	ft_atoi(const char *str)
+long	ft_atol(const char *str)
 {
-	size_t			i;
-	unsigned long	nb;
-	int				sign;
+	size_t				i;
+	unsigned long long	nb;
+	int					sign;
 
 	i = 0;
 	sign = 1;
@@ -34,11 +34,14 @@ int	ft_atoi(const char *str)
 	while (ft_isspace(str[i]))
 		i++;
 	if (ft_issign(str[i]) != 0)
-		sign *= ft_issign(str[i++]);
-	while (ft_isdigit(str[i]) && nb <= INT_MAX)
+	{
+		sign *= ft_issign(str[i]);
+		i++;
+	}
+	while (ft_isdigit(str[i]))
 	{
 		nb = nb * 10 + (str[i] - '0');
 		i++;
 	}
-	return (nb * sign);
+	return ((long)(nb * sign));
 }
