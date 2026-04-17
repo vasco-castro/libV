3276269f749dd3517f639af8a4699f731f321ae2
Vasco Castro <vcastro@gmtel.pt>
2026-04-17 10:03:33 +0100

    Add ft_free function and prototype, a safer wraper of free, it only frees if pointer is not NULL

--- include/memory.h
+++ include/memory.h
@@ -23,4 +23,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *b, int c, size_t len);
 
+void	ft_free(void *ptr);
+
 #endif
